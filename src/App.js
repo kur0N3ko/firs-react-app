c7188fc1f0b019f9eaa07bfb0d12521168c445c0
dxojt <dxojt@dxlabss-Mac-mini.local>
2024-04-15 08:34:33 +0800

    DataRows added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import './App.css';
 import { NavbarProvider } from "./components/NavbarContext";
-import { CrudProvider, useCrudContext} from "./components/CrudContext";
+import { CrudProvider} from "./components/CrudContext";
 import Form from "./components/Form";
-import DeleteData from "./components/DeleteData";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import DataRows from "./components/DataRows";
 
 function App() {
 
@@ -29,49 +29,47 @@ function App() {
     </Router>
     <div className="Form">
       <CrudProvider>
-      <h1>Scheduling System</h1>
-      <Form/>
-        <hr/>
-        <table>
-          <thead>
-            <tr>
-              <th>Bus Name</th>
-              <th>Starting Point</th>
-              <th>Time</th>
-              <th>End Point</th>
-              <th>Time</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-            <tbody>
-              <DataRows/>
-            </tbody>
-        </table>
-      
+        <h1>Scheduling System</h1>
+          <Form/>
+            <hr/>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Bus Name</th>
+                    <th>Starting Point</th>
+                    <th>Time</th>
+                    <th>End Point</th>
+                    <th>Time</th>
+                    <th>Date</th>
+                  </tr>
+                </thead>
+                  <tbody>
+                    <DataRows/>
+                  </tbody>
+              </table>
       </CrudProvider>
     </div>
   </>
   );
 }
 
-function DataRows() {
-  const { data } = useCrudContext();
+// function DataRows() {
+//   const { data } = useCrudContext();
 
 
-  return data.map(item => (
-    <tr key={item.id}>
-      <td>{item.name}</td>
-      <td>{item.spoint}</td>
-      <td>{item.time}</td>
-      <td>{item.epoint}</td>
-      <td>{item.etime}</td>
-      <td>{item.date}</td>
-      <td>
-        <DeleteData id={item.id} />
-        {/* <button onClick={() => handleEdit(item.id)}>Edit</button> */}
-      </td>
-    </tr>
-  ));
-}
+//   return data.map(item => (
+//     <tr key={item.id}>
+//       <td>{item.name}</td>
+//       <td>{item.spoint}</td>
+//       <td>{item.time}</td>
+//       <td>{item.epoint}</td>
+//       <td>{item.etime}</td>
+//       <td>{item.date}</td>
+//       <td>
+//         <DeleteData id={item.id} />
+//       </td>
+//     </tr>
+//   ));
+// }
 
 export default App;
